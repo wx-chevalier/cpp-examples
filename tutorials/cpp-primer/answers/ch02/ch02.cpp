b5281b18702d127fc95f108e9185f7aa9d3ceb0e
wx-chevalier <384924552@qq.com>
2023-06-17 09:21:51 +0800

    feat: update articles or snippets

--- tutorials/cpp-primer/answers/ch02/ch02.cpp
+++ tutorials/cpp-primer/answers/ch02/ch02.cpp
@@ -6,13 +6,15 @@
 #include "Sales_item.h"
 #include <iostream>
 
-void basic_class() {
+void basic_class()
+{
   Sales_item book;
   std::cin >> book;
   std::cout << book << std::endl;
 }
 
-void q_2_3() {
+void q_2_3()
+{
   unsigned u = 10, u2 = 42;
   std::cout << u2 - u << std::endl;
   std::cout << u - u2 << std::endl;
@@ -30,13 +32,15 @@ void q_2_3() {
 #include <iostream>
 #include <string>
 
-struct Sale_data {
+struct Sale_data
+{
   std::string bookNo;
   unsigned units_sold = 0;
   double revenue = 0.0;
 };
 
-int q_1_5_1() {
+int q_1_5_1()
+{
   Sale_data book;
   double price;
   std::cin >> book.bookNo >> book.units_sold >> price;
@@ -48,7 +52,8 @@ int q_1_5_1() {
 }
 
 // 1.5.2
-int q_1_5_2() {
+int q_1_5_2()
+{
   Sale_data book1, book2;
   double price1, price2;
   std::cin >> book1.bookNo >> book1.units_sold >> price1;
@@ -56,7 +61,8 @@ int q_1_5_2() {
   book1.revenue = book1.units_sold * price1;
   book2.revenue = book2.units_sold * price2;
 
-  if (book1.bookNo == book2.bookNo) {
+  if (book1.bookNo == book2.bookNo)
+  {
     unsigned totalCnt = book1.units_sold + book2.units_sold;
     double totalRevenue = book1.revenue + book2.revenue;
     std::cout << book1.bookNo << " " << totalCnt << " " << totalRevenue << " ";
@@ -65,28 +71,36 @@ int q_1_5_2() {
     else
       std::cout << "(no sales)" << std::endl;
     return 0;
-  } else {
+  }
+  else
+  {
     std::cerr << "Data must refer to same ISBN" << std::endl;
     return -1; // indicate failure
   }
 }
 
 // 1.6
-int q_1_6() {
+int q_1_6()
+{
   Sale_data total;
   double totalPrice;
-  if (std::cin >> total.bookNo >> total.units_sold >> totalPrice) {
+  if (std::cin >> total.bookNo >> total.units_sold >> totalPrice)
+  {
     total.revenue = total.units_sold * totalPrice;
 
     Sale_data trans;
     double transPrice;
-    while (std::cin >> trans.bookNo >> trans.units_sold >> transPrice) {
+    while (std::cin >> trans.bookNo >> trans.units_sold >> transPrice)
+    {
       trans.revenue = trans.units_sold * transPrice;
 
-      if (total.bookNo == trans.bookNo) {
+      if (total.bookNo == trans.bookNo)
+      {
         total.units_sold += trans.units_sold;
         total.revenue += trans.revenue;
-      } else {
+      }
+      else
+      {
         std::cout << total.bookNo << " " << total.units_sold << " "
                   << total.revenue << " ";
         if (total.units_sold != 0)
@@ -108,16 +122,19 @@ int q_1_6() {
       std::cout << "(no sales)" << std::endl;
 
     return 0;
-  } else {
+  }
+  else
+  {
     std::cerr << "No data?!" << std::endl;
     return -1; // indicate failure
   }
 }
 
-int main() {
-  //    q_2_3();
+int main()
+{
+  q_2_3();
   //    q_1_5_1();
   //    q_1_5_2();
-  q_1_6();
+  // q_1_6();
   return 0;
 }
